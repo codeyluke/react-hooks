5d3408b367b63f26df388d31e063ef67ed69986b
luke <lukekennedy212@gmail.com>
2020-10-11 13:27:06 +0800

    useContext part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import DataFetching from './components/DataFetching';
+import ComponentC from './components/ComponentC';
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={'beast'}>
+        <ChannelContext.Provider value={'beastnation'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
