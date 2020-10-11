86a886eed8fffdf783f2447206a91ebd7366ae95
luke <lukekennedy212@gmail.com>
2020-10-11 12:35:10 +0800

    fetching part 3

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -4,19 +4,25 @@ import axios from 'axios'
 function DataFetching() {
   const [post, setPost] = useState({})
   const [id, setId] = useState(1)
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1)
 
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then(res => {
         console.log(res)
         setPost(res.data)
       })
       .catch(err => { console.log(err) })
-  }, [id])
+  }, [idFromButtonClick])
+
+  const handleClick = () => {
+    setIdFromButtonClick(id)
+  }
 
   return (
     <div>
       <input type="text" value={id} onChange={e => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>Fetch Post</button>
       <div>
         {post.title}
       </div>
